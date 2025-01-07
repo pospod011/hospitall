dddd62fdb41cee48b52d6ba688244c38320b0b5b
unknown <you@example.com>
2025-01-07 12:49:12 +0600

    frize

--- mysiti/hosp/permissions.py
+++ mysiti/hosp/permissions.py
@@ -1,29 +1,29 @@
-from rest_framework import permissions
-from .models import PatientProfile, Doctor
-
-
-
-
-class CheckDoctor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        user = request.user
-        if isinstance(user, Doctor) and user.role == 'врач':
-            return True
-        return False
-
-
-class CheckPatient(permissions.BasePermission):
-    def has_permission(self, request, view):
-        user = request.user
-        if isinstance(user, PatientProfile) and user.role == 'пациент':
-            return True
-        return False
-
-
-class CheсkReview(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.patient == request.user
-
-
+# from rest_framework import permissions
+# from .models import PatientProfile, Doctor
+# 
+# 
+#
+# 
+# class CheckDoctor(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         user = request.user
+#         if isinstance(user, Doctor) and user.role == 'врач':
+#             return True
+#         return False
+# 
+# 
+# class CheckPatient(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         user = request.user
+#         if isinstance(user, PatientProfile) and user.role == 'пациент':
+#             return True
+#         return False
+# 
+# 
+# class CheсkReview(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         return obj.patient == request.user
+# 
+# 
